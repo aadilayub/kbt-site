554c20fae2ba9e90b1c07d4103d313c765232b24
Aadil Ayub <aadilayub@protonmail.com>
2022-09-15 06:18:43 +0200

    use wp_head for all meta tags

--- header.php
+++ header.php
@@ -1,22 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Karachi Bachao Tehreek</title>
-
-  <meta property="og:site_name" content="Karachi Bachao Tehreek">
-  <meta name="author" content="Karachi Bachao Tehreek">
-  <meta name="description" content="Karachi Bachao Tehreek is a campaign to end the demolition of Karachi's working class settlements and markets.">
-
-  <meta property="og:title" content="Karachi Bachao Tehreek">
-  <meta property="og:description" content="Karachi Bachao Tehreek is a campaign to end the demolition of Karachi's working class settlements and markets.">
-  
-  <meta property="og:image" content="https://karachibachaotehreek.org/wp-content/themes/karachibachaotehreek/assets/img/logo.jpg">
-  <meta property="og:url" content="https://karachibachaotehreek.org">
-
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:site" content="@StopEvictionKHI">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>

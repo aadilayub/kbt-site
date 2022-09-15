2c564e2bcd342f64c0d2e29266414e1bb69c6c73
Aadil Ayub <aadilayub@protonmail.com>
2022-09-15 09:01:03 +0200

    add title tag support to wordpress theme

--- functions.php
+++ functions.php
@@ -6,4 +6,5 @@ function kbt_add_styles() {
 
 add_action( 'wp_enqueue_scripts', 'kbt_add_styles' );
 
-add_theme_support('post_thumbnails');
+add_theme_support( 'post_thumbnails' );
+add_theme_support( 'title-tag' );

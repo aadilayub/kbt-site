a18ebb9b8f6026c4a6128d7415ef7e02b0df2617
Aadil Ayub <aadilayub@protonmail.com>
2022-09-15 17:05:19 +0200

    add more meta attributes via php

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
+   <meta charset ="<?php bloginfo( 'charset' );  ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
